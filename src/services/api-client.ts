df7691d3472eb920b40c5c1d08337b32292dd81a
jgill1029 <jgill1029@gmail.com>
2023-09-12 10:45:34 -0400

    Create a cusom hook to fetch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api/',
     params: {
-        key: '2fbb7c8b1ed345048af87ce36439eed6'
+        key: ''
+        //4e45decceaa840d9ac68043064723e34
     }
 })
