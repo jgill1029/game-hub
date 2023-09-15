c04091235979bde64e47a63cfe884210c110281a
jgill1029 <jgill1029@gmail.com>
2023-09-14 20:20:09 -0400

    Correct Genre Fetch

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api/',
     params: {
-        key: '4e45decceaa840d9ac68043064723e34'
-        //
+        key: ''
+        //4e45decceaa840d9ac68043064723e34
     }
 })
