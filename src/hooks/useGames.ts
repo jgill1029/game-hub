9ad53ed93e961f6de24d1b1a47a02ee499de2c61
jgill1029 <jgill1029@gmail.com>
2023-09-17 18:11:55 -0400

    Fixed platform fetch bug

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,4 @@
 import { GameQuery } from "../App";
-import { Genre } from "./useGenres";
 import useData from "./useData";
 
 export interface Platform {
@@ -22,7 +21,7 @@ const useGames = (gameQuery: GameQuery) =>
 useData<Game>('/games', { 
   params: { 
     genres: gameQuery.genre?.id, 
-    platforms: gameQuery.platform?.id,
+    parent_platforms: gameQuery.platform?.id,
     ordering: gameQuery.sortOrder,
     search: gameQuery.searchText
   }}, 
